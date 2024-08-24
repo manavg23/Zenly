db2793b02c6ffa5f7e9886ed79e1a1397dd8ef39
manavg23 <86246951+manavg23@users.noreply.github.com>
2024-08-24 15:57:04 -0400

    Delete fe-05-2-nutrition-tracker directory
    delete 1

--- fe-05-2-nutrition-tracker/public/src/js/lib/es6-polyfill.js
+++
@@ -1,9 +0,0 @@
-/*! Polyfill for ES6 Number.isInteger */
-/* source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger */
-
-Number.isInteger = Number.isInteger || function(value) {
-  return typeof value === "number" &&
-    isFinite(value) &&
-    Math.floor(value) === value;
-};
-
